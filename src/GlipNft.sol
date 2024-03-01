5fa1bf14fc8e821925a71de936c1a1c00981ded8
alex <enna@mailbox.org>
2024-03-01 18:45:32 +0100

    Support royalties ...

--- src/GlipNft.sol
+++ src/GlipNft.sol
@@ -4,21 +4,39 @@ pragma solidity ^0.8.0;
 import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+
 /**
  * @title GlipNft.sol NFT Contract
  */
-contract GlipNft is ERC721URIStorage, Ownable2Step {
+contract GlipNft is ERC721URIStorage, ERC721Royalty, Ownable2Step {
 
     uint256 private _currentTokenId = 0;
 
     constructor() ERC721("GlipNft", "GLN") Ownable(address(0x977583aA8aaFEE37f60A1eee8eB273a1dF05F57c)) {}
 
-    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
+    function mintNFT(address recipient, string memory aTokenURI) public onlyOwner returns (uint256) {
         _currentTokenId += 1;
         uint256 newItemId = _currentTokenId;
         _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _setTokenURI(newItemId, aTokenURI);
         return newItemId;
     }
+
+
+    function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721URIStorage, ERC721Royalty)
+    returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    // Explicitly override tokenURI to use ERC721URIStorage's implementation
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
 
 }
--- test/GlipNftTest.sol
+++ test/GlipNftTest.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/GlipNft.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
 /**
  * @title Test suite for the GlipNft.sol NFT contract
@@ -38,6 +41,34 @@ contract GlipNftTest is Test {
         vm.stopPrank();
     }
 
+    function testSupportsInterface() public {
+        // ERC721 interface ID
+        bytes4 erc721InterfaceId = type(IERC721).interfaceId;
+        assertTrue(nftTest.supportsInterface(erc721InterfaceId), "Should support ERC721 interface");
+
+        // ERC721Metadata (URIStorage) interface ID
+        bytes4 erc721MetadataInterfaceId = type(IERC721Metadata).interfaceId;
+        assertTrue(nftTest.supportsInterface(erc721MetadataInterfaceId), "Should support ERC721Metadata interface");
+
+        // ERC721Royalty interface ID
+        bytes4 erc2981InterfaceId = type(IERC2981).interfaceId;
+        assertTrue(nftTest.supportsInterface(erc2981InterfaceId), "Should support ERC2981 interface");
+
+        // An arbitrary interface ID not supported by the contract
+        bytes4 fakeInterfaceId = 0xdeadbeef;
+        assertFalse(nftTest.supportsInterface(fakeInterfaceId), "Should not support arbitrary interface");
+    }
+    function testTokenURI() public {
+        string memory testURI = "https://example.com/token1";
+        // Mint a new NFT
+        vm.startPrank(owner);
+        uint256 tokenId = nftTest.mintNFT(owner, testURI);
+        vm.stopPrank();
+
+        // Retrieve the token URI and compare
+        string memory retrievedURI = nftTest.tokenURI(tokenId);
+        assertEq(retrievedURI, testURI, "The retrieved URI should match the test URI");
+    }
 
 
 
